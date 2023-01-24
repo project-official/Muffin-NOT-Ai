bf32ecefadbf3d993e71043601d7bac9d2e39aaf
Migan178 <migan9857@gmail.com>
2023-01-24 20:03:46 +0900

    fix: remove dokdo

--- src/Client.ts
+++ src/Client.ts
@@ -2,7 +2,6 @@ import { ActivityType, Client, GatewayIntentBits } from 'discord.js'
 import ChatBot from './ChatBot.js'
 import { join, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import Dokdo from 'dokdo'
 import 'dotenv/config'
 
 export default class MuffinAI extends Client {
@@ -29,12 +28,6 @@ export default class MuffinAI extends Client {
       console.log(`먹힐 준비 완료`)
     }).on('messageCreate', msg => {
       if (msg.author.bot) return
-      new Dokdo(this, {
-        prefix: '멒힌아 ',
-        noPerm: msg => msg.reply('당신은 내 남자친구가 아니잖아!'),
-        aliases: ['테스트'],
-        owners: ['415135882006495242'],
-      }).run(msg)
       if (msg.content.startsWith('머핀아 ')) this.chatBot.getResponse(msg, true)
       else if (msg.content.startsWith('멒힌아 봇꺼')) {
         if (msg.author.id !== '415135882006495242') {
