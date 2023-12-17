fd300623988b24040831915feae5cce38a30f477
Migan178 <migan9857@gmail.com>
2023-12-17 20:35:37 +0900

    .

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
     `statement` (
-        `id` int(11) NOT NULL,
+        `id` int(11) NOT NULL AUTO_INCREMENT,
         `text` varchar(255) DEFAULT NULL,
         `search_text` varchar(255) NOT NULL DEFAULT '',
         `conversation` varchar(32) NOT NULL DEFAULT '',
@@ -13,7 +13,7 @@ CREATE TABLE
 
 CREATE TABLE
     `nsfw_content` (
-        `id` int(11) NOT NULL,
+        `id` int(11) NOT NULL AUTO_INCREMENT,
         `text` varchar(255) NOT NULL DEFAULT '',
         `created_at` datetime DEFAULT current_timestamp(),
         `persona` varchar(50) NOT NULL DEFAULT '',
@@ -21,10 +21,11 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    learn (
-        command varchar(255) NOT NULL,
-        result varchar(255) NOT NULL,
-        user_id varchar(255) NOT NULL,
-        created_at datetime NOT NULL DEFAULT current_timestamp(),
-        primary key(`command`)
+    `learn` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `command` varchar(255) NOT NULL,
+        `result` varchar(255) NOT NULL,
+        `user_id` varchar(255) NOT NULL,
+        `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+        primary key(`id`)
     );
