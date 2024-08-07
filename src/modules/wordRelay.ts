a12d13ead473e7f9dd739b961777d6e791832a7c
Migan178 <me@migan.co.kr>
2024-08-07 18:13:25 +0900

    fix: ignore promise & function name

--- src/modules/wordRelay.ts
+++ src/modules/wordRelay.ts
@@ -49,7 +49,7 @@ export class WordRelay {
           name: `${container.client.user?.username}-끝말잇기`,
         })
         .then(thread => {
-          thread.send(
+          void thread.send(
             `<@${userID}>님, 여기 들어와서 시작단어를 60초안에 입력해주세요!`,
           )
 
@@ -58,50 +58,50 @@ export class WordRelay {
             time: 60_000,
           })
 
-          collector
-            .on('collect', async message => {
-              if (message.content.length < 2) {
-                await message.reply(
-                  '해당 단어는 너무 짧아요. 다시 한번 입력해주세요.',
-                )
-              } else if (!(await this.validWord(message.content))) {
-                await message.reply(
-                  '해당 단어는 일치하지 않아요. 다시 한번 입력해주세요.',
-                )
-              } else if (
-                !(await this.getWord(
-                  message.content.slice(message.content.length - 1),
-                ))
-              ) {
-                await message.reply(
-                  '시작단어가 한방단어면 안돼요. 다시 한번 입력해주세요.',
-                )
-              } else {
-                collector.stop(`${BBWR_COLLECTED}: ${message.content}`)
-              }
-            })
-            .on('end', (_, reason) => {
-              if (reason === 'time') {
-                thread.send(
-                  `<@${userID}>님, 60초동안 시작단어를 입력하지 않아 자동으로 게임이 종료되었어요.`,
-                )
-              } else if (reason.startsWith(BBWR_COLLECTED)) {
-                this._usedWords.push(reason.slice(`${BBWR_COLLECTED}: `.length))
-              }
-            })
+          collector.on('collect', async message => {
+            if (message.content.length < 2) {
+              await message.reply(
+                '해당 단어는 너무 짧아요. 다시 한번 입력해주세요.',
+              )
+            } else if (!(await this.validWord(message.content))) {
+              await message.reply(
+                '해당 단어는 일치하지 않아요. 다시 한번 입력해주세요.',
+              )
+            } else if (
+              !(await this.getWord(
+                message.content.slice(message.content.length - 1),
+              ))
+            ) {
+              await message.reply(
+                '시작단어가 한방단어면 안돼요. 다시 한번 입력해주세요.',
+              )
+            } else {
+              collector.stop(`${BBWR_COLLECTED}: ${message.content}`)
+            }
+          })
+
+          collector.on('end', (_, reason) => {
+            if (reason === 'time') {
+              void thread.send(
+                `<@${userID}>님, 60초동안 시작단어를 입력하지 않아 자동으로 게임이 종료되었어요.`,
+              )
+            } else if (reason.startsWith(BBWR_COLLECTED)) {
+              this._usedWords.push(reason.slice(`${BBWR_COLLECTED}: `.length))
+            }
+          })
         })
     } catch (err) {
       console.error(err)
     }
   }
 
-  private _getrandomWord(wordList: APIResponse[] | null): APIResponse | null {
+  private _getRandomWord(wordList: APIResponse[] | null): APIResponse | null {
     if (!wordList) return null
 
     const wordInfo = wordList[Math.floor(Math.random() * wordList.length)]
 
     if (this._usedWords.includes(wordInfo.word))
-      return this._getrandomWord(wordList)
+      return this._getRandomWord(wordList)
     else return wordInfo
   }
 
@@ -121,6 +121,6 @@ export class WordRelay {
       },
     }).then(res => res.body.json())
 
-    return this._getrandomWord(res.channel.item)
+    return this._getRandomWord(res.channel.item)
   }
 }
