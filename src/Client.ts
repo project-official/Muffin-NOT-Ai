0fcff7207fc7e9fb882003c030176e5c0aaf08c4
Siwoo Jeon <me@migan.co.kr>
2024-10-04 21:57:45 +0900

    fix: log

--- src/Client.ts
+++ src/Client.ts
@@ -60,11 +60,12 @@ export default class MuffinBot extends SapphireClient {
 
   public override async login(): Promise<string> {
     if (container.channel === 'RELEASE') await container.chatBot.train(this)
-    else
+    else {
       container.logger.info(
-        '[MuffinBot] 해당 채널은 RELEASE 채널이 아니라서 학습 기능이 꺼졌습니다.\n' +
-          `현재 채널: ${container.channel}`,
+        '[MuffinBot] 해당 채널은 RELEASE 채널이 아니라서 학습 기능이 꺼졌습니다.',
       )
+      container.logger.info(`[MuffinBot] 현재 채널: ${container.channel}`)
+    }
     return super.login(config.bot.token)
   }
 }
